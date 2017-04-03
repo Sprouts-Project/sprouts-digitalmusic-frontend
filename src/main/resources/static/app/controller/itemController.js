28ac22783f5e374c8e7c04628a26aa023a9191a5
jjxp <javgarcal@alum.us.es>
2017-04-03 12:39:49 +0200

    Pagination support
    The frontend has been provided with support for pagination

--- src/main/resources/static/app/controller/itemController.js
+++ src/main/resources/static/app/controller/itemController.js
@@ -2,17 +2,44 @@
 (function() {
 	var ItemController = function($scope, $http) {
 		$scope.items = [],
+		$scope.paginationInfo = {
+				currentPage : '0',
+				minPage : '0',
+				maxPage : '5'
+		},
+  		$scope.range = function() {
+			var result = [];
+			for (var i = $scope.paginationInfo.minPage; i < $scope.paginationInfo.maxPage; i++) {
+				result.push(i);
+			}
+			return result;
+		},
 		$scope.error = false;
-
-		self.doGetItems = function() {
-			$http.get('/item/list').then(function(response) {
-				$scope.items = response.data;
+		
+		$scope.reloadPaginated = function(page) {
+			$scope.paginationInfo.currentPage = page;
+			console.log(page);
+			init();
+		}
+		
+		self.doGetItems = function(pageNumber) {
+			$http({
+				method : 'GET',
+				url : '/item/list',
+				params : {
+					pageNumber : pageNumber
+				}
+			}).success(function(response) {
+				$scope.items = response[0];
+				$scope.paginationInfo = response[1];
 				$scope.error = false;
-			});
+			}).error(function (response) {
+				$scope.error = true;
+	        });
 		}
 
 		function init() {
-			self.doGetItems();
+			self.doGetItems($scope.paginationInfo.currentPage);
 		}
 		init();
 
