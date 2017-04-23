ba192793ce2e3083cd761351c1669dd2e4a58239
David Arroyo Escobar <arroyoescobardavid@gmail.com>
2017-04-23 15:01:33 -0700

    Added select to sales by state

--- src/main/resources/static/admin/app/pages/dashboard/overviewFinance/OverviewFinanceCtrl.js
+++ src/main/resources/static/admin/app/pages/dashboard/overviewFinance/OverviewFinanceCtrl.js
@@ -9,7 +9,7 @@
       .controller('OverviewFinanceCtrl', OverviewFinanceCtrl);
 
   /** @ngInject */
-  function OverviewFinanceCtrl($http, orderByFilter, baConfig, layoutPaths) {
+  function OverviewFinanceCtrl($scope, $http, orderByFilter, baConfig, layoutPaths) {
 	  
 	  function getColor(data, min, max, step){
 	  		var color = '#4169E1';
@@ -27,6 +27,37 @@
 	  		}
 	  		return color;
 	  	}
+	  
+	  // Chart states
+	  function setChartOverviewFinanceMap(areas){
+		  var map = AmCharts.makeChart('overviewFinanceMap', {
+		      type: 'map',
+		      theme: 'blur',
+		      zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
+		
+		      dataProvider: {
+		        map: 'usaLow',
+		        zoomLevel: 1,
+		        zoomLongitude: 10,
+		        zoomLatitude: 52,
+		        areas: areas
+		      },
+		
+		      areasSettings: {
+		        rollOverOutlineColor: layoutColors.border,
+		        rollOverColor: layoutColors.primaryDark,
+		        alpha: 0.8,
+		        unlistedAreasAlpha: 0.2,
+		        unlistedAreasColor: layoutColors.defaultText,
+		        balloonText: '[[title]]: [[customData]] monthly sale'
+		      },
+		      export: {
+		        enabled: true
+		      },
+		      creditsPosition: 'bottom-right',
+		      pathToImages: layoutPaths.images.amChart
+		    });
+	  }
 	  
     var layoutColors = baConfig.colors;
     
@@ -86,48 +117,53 @@
     	});
     	
     	// Month sales by state
-    	var areas = [], max = 0, min = -1, step = 0;
-    	response.data.monthly_sales_by_state.forEach(function(item, index){
-    		var val = item.value;
-    		max = (val > max)?val : max;
-    		min = (min < 0 || min > val)? val : min;
-    	});
     	
-    	step = (max-min) / 6;
+    	var dataArr = response.data.monthly_sales_by_state;
 
-    	response.data.monthly_sales_by_state.forEach(function(item, index){
-    		var color = getColor(item.value, min, max, step);
-    		areas.push({ title: item.name, color: color, id: item.abbreviation, customData: parseFloat(item.value).toFixed(2) });
+		var month = 1;
+		var year = 2018;
 
-    	});
-    	
-	    var map = AmCharts.makeChart('overviewFinanceMap', {
-	      type: 'map',
-	      theme: 'blur',
-	      zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
-	
-	      dataProvider: {
-	        map: 'usaLow',
-	        zoomLevel: 1,
-	        zoomLongitude: 10,
-	        zoomLatitude: 52,
-	        areas: areas
-	      },
-	
-	      areasSettings: {
-	        rollOverOutlineColor: layoutColors.border,
-	        rollOverColor: layoutColors.primaryDark,
-	        alpha: 0.8,
-	        unlistedAreasAlpha: 0.2,
-	        unlistedAreasColor: layoutColors.defaultText,
-	        balloonText: '[[title]]: [[customData]] monthly sale'
-	      },
-	      export: {
-	        enabled: true
-	      },
-	      creditsPosition: 'bottom-right',
-	      pathToImages: layoutPaths.images.amChart
-	    });
+		var options = [];
+		for (var i = 0; i < dataArr.length; i++) {
+			
+			var statesSales = dataArr[i].statesSales;
+			
+			var areas = [], max = 0, min = -1, step = 0;
+			statesSales.forEach(function(item, index){
+	    		var val = item.sales;
+	    		max = (val > max)?val : max;
+	    		min = (min < 0 || min > val)? val : min;
+	    	});
+	    	
+	    	step = (max-min) / 6;
+
+	    	statesSales.forEach(function(item, index){
+	    		var color = getColor(item.sales, min, max, step);
+	    		areas.push({ title: item.state, color: color, id: item.abbreviation, customData: parseFloat(item.sales).toFixed(2) });
+
+	    	});
+			
+			options.push({
+				name : dataArr[i].month + "/" + dataArr[i].year,
+				month : dataArr[i].month,
+				year : dataArr[i].year,
+				salesStates : areas
+			});
+		}
+		
+		$scope.dropDownFinanceOptions = options.sort(function(a, b) {
+			return new Date(a.year, a.month, 1)
+					- new Date(b.year, b.month, 1);
+		});
+
+		$scope.selectedFinanceOption = options[0];
+		
+		setChartOverviewFinanceMap(options[0].salesStates);
+		
+		$scope.updateFinance = function() {
+			var data = $scope.selectedFinanceOption.salesStates;
+			setChartOverviewFinanceMap(data)
+		}
     });
   }
 })();
--- src/main/resources/static/admin/app/pages/dashboard/overviewStock/OverviewStockCtrl.js
+++ src/main/resources/static/admin/app/pages/dashboard/overviewStock/OverviewStockCtrl.js
@@ -11,8 +11,37 @@
   /** @ngInject */
   function OverviewStockCtrl($scope, $http, orderByFilter, baConfig, layoutPaths, baUtil) {
     var layoutColors = baConfig.colors;
-    var graphColor = baConfig.theme.blur ? '#000000' : layoutColors.primary;
     
+    function setChartOverviewStockMap(areas){
+    	//Month sales by state
+	    var map = AmCharts.makeChart('overviewStockMap', {
+	      type: 'map',
+	      theme: 'blur',
+	      zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
+	
+	      dataProvider: {
+	        map: 'usaLow',
+	        zoomLevel: 1,
+	        zoomLongitude: 10,
+	        zoomLatitude: 52,
+	        areas: areas
+	      },
+	
+	      areasSettings: {
+	        rollOverOutlineColor: layoutColors.border,
+	        rollOverColor: layoutColors.primaryDark,
+	        alpha: 0.8,
+	        unlistedAreasAlpha: 0.2,
+	        unlistedAreasColor: layoutColors.defaultText,
+	        balloonText: '[[title]]: [[customData]] monthly sale'
+	      },
+	      export: {
+	        enabled: true
+	      },
+	      creditsPosition: 'bottom-right',
+	      pathToImages: layoutPaths.images.amChart
+	    });
+    }
     function getColor(data, min, max, step){
   		var color = '#4169E1';
   		
@@ -85,50 +114,52 @@
     	});
     	
     	//Month sales
-    	var areas = [], max = 0, min = -1, step = 0;
-    	
-    	response.data.monthly_sales_by_state.forEach(function(item, index){
-    		var val = item.numProducts;
-    		max = (val > max)?val : max;
-    		min = (min < 0 || min > val)? val : min;
-    	});
-    	
-    	step = (max-min) / 6;
+    	var dataArr = response.data.monthly_sales_by_state;
 
-    	response.data.monthly_sales_by_state.forEach(function(item, index){
-    		var color = getColor(item.numProducts, min, max, step);
-    		areas.push({ title: item.name, color: color, id: item.abbreviation, customData: parseFloat(item.numProducts).toFixed(2) });
+		var month = 1;
+		var year = 2018;
 
-    	});
-    	
-    	//Month sales by state
-	    var map = AmCharts.makeChart('overviewStockMap', {
-	      type: 'map',
-	      theme: 'blur',
-	      zoomControl: { zoomControlEnabled: false, panControlEnabled: false },
-	
-	      dataProvider: {
-	        map: 'usaLow',
-	        zoomLevel: 1,
-	        zoomLongitude: 10,
-	        zoomLatitude: 52,
-	        areas: areas
-	      },
-	
-	      areasSettings: {
-	        rollOverOutlineColor: layoutColors.border,
-	        rollOverColor: layoutColors.primaryDark,
-	        alpha: 0.8,
-	        unlistedAreasAlpha: 0.2,
-	        unlistedAreasColor: layoutColors.defaultText,
-	        balloonText: '[[title]]: [[customData]] monthly sale'
-	      },
-	      export: {
-	        enabled: true
-	      },
-	      creditsPosition: 'bottom-right',
-	      pathToImages: layoutPaths.images.amChart
-	    });
+		var options = [];
+		for (var i = 0; i < dataArr.length; i++) {
+			
+			var statesSales = dataArr[i].statesSales;
+			
+			var areas = [], max = 0, min = -1, step = 0;
+			statesSales.forEach(function(item, index){
+	    		var val = item.sales;
+	    		max = (val > max)?val : max;
+	    		min = (min < 0 || min > val)? val : min;
+	    	});
+	    	
+	    	step = (max-min) / 6;
+
+	    	statesSales.forEach(function(item, index){
+	    		var color = getColor(item.sales, min, max, step);
+	    		areas.push({ title: item.state, color: color, id: item.abbreviation, customData: parseFloat(item.sales).toFixed(2) });
+
+	    	});
+			
+			options.push({
+				name : dataArr[i].month + "/" + dataArr[i].year,
+				month : dataArr[i].month,
+				year : dataArr[i].year,
+				salesStates : areas
+			});
+		}
+		
+		$scope.dropDownStockOptions = options.sort(function(a, b) {
+			return new Date(a.year, a.month, 1)
+					- new Date(b.year, b.month, 1);
+		});
+
+		$scope.selectedStockOption = options[0];
+		
+		setChartOverviewStockMap(options[0].salesStates);
+		
+		$scope.updateStock = function() {
+			var data = $scope.selectedStockOption.salesStates;
+			setChartOverviewStockMap(data)
+		}	
 	    
 	    //Table top product
 	    var topProducts = [];
