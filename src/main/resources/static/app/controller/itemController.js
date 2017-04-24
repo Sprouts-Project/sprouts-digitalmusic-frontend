84a56de106f7194881f13bc30fe3b76e09d481c1
jjxp <javiergc01@gmail.com>
2017-04-24 18:57:35 +0200

    Fixed pagination issue

--- src/main/resources/static/app/controller/itemController.js
+++ src/main/resources/static/app/controller/itemController.js
@@ -1,8 +1,7 @@
 'use strict';
 (function() {
 	var ItemController = function($scope, $http, AuthService) {
-		$scope.items = [],
-		$scope.reviews = [],
+        $scope.items = [],
 		$scope.paginationInfo = {
 				currentPage : '0',
 				minPage : '0',
@@ -21,25 +20,21 @@
 			$scope.paginationInfo.currentPage = page;
 			init();
 		}
-		
-		self.doGetItems = function(pageNumber) {
-			$http({
-				method : 'GET',
-				url : '/item/list',
-				params : {
-					pageNumber : pageNumber
-				}
-			}).success(function(response) {
-				$scope.items = response[0];
-				$scope.paginationInfo = response[1];
-				$scope.error = false;
-			}).error(function (response) {
-				$scope.error = true;
-	        });
-		}
 
 		function init() {
-			self.doGetItems($scope.paginationInfo.currentPage);
+            $http({
+                method : 'GET',
+                url : '/item/list',
+                params : {
+                    pageNumber : $scope.paginationInfo.currentPage
+                }
+            }).success(function(response) {
+                $scope.items = response[0];
+                $scope.paginationInfo = response[1];
+                $scope.error = false;
+            }).error(function (response) {
+                $scope.error = true;
+            });
 		}
 		init();
 
