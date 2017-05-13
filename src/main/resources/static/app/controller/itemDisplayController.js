f61d98bf0b6a9b511a8eccb4636f7c10a33b91a9
Daniel Moreno Márquez <danimoreno.m08@gmail.com>
2017-05-13 12:45:05 +0200

    Load more reviews in details of item

--- src/main/resources/static/app/controller/itemDisplayController.js
+++ src/main/resources/static/app/controller/itemDisplayController.js
@@ -2,9 +2,14 @@
 (function () {
     var ItemDisplayController = function ($scope, $http, $routeParams, $filter, AuthService) {
         $scope.item,
-        $scope.showForm = false,
-        $scope.reviews = [],
-        $scope.average = 0.;
+            $scope.showForm = false,
+            $scope.all_reviews = [],
+            $scope.average = 0.,
+            $scope.reviews = [],
+            $scope.counterReview = 1,
+            $scope.showLoadMore = true;
+
+        const NUMBER_OF_REVIEWS = 5;
 
         self.doGetItem = function (id) {
             $http({
@@ -30,14 +35,18 @@
                     itemId: id
                 }
             }).success(function (response) {
-                $scope.reviews = response;
+                $scope.all_reviews = response;
+                $scope.reviews = $scope.all_reviews.slice(0, NUMBER_OF_REVIEWS);
+                if ($scope.reviews.length < NUMBER_OF_REVIEWS || $scope.all_reviews.length == NUMBER_OF_REVIEWS) {
+                    $scope.showLoadMore = false;
+                }
 
-                if($scope.reviews.length > 0) {
+                if ($scope.all_reviews.length > 0) {
                     var sum = 0;
-                    for (var i = 0; i < $scope.reviews.length; i++) {
-                        sum += parseInt($scope.reviews[i].overall, 10);
+                    for (var i = 0; i < $scope.all_reviews.length; i++) {
+                        sum += parseInt($scope.all_reviews[i].overall, 10);
                     }
-                    var avg = sum / $scope.reviews.length;
+                    var avg = sum / $scope.all_reviews.length;
 
                     $scope.average = avg;
                 }
@@ -98,7 +107,22 @@
                 $scope.error = true;
             });
         }
-        
+
+        self.loadmore = function () {
+            var index = $scope.counterReview * NUMBER_OF_REVIEWS;
+            var sublist = $scope.all_reviews.slice(index, index + NUMBER_OF_REVIEWS);
+            if (sublist.length < NUMBER_OF_REVIEWS || $scope.all_reviews.length == index + NUMBER_OF_REVIEWS) {
+                $scope.showLoadMore = false;
+            }
+
+            var i;
+            for (i = 0; i < sublist.length; i++) {
+                $scope.reviews.push(sublist[i]);
+            }
+            $scope.counterReview = $scope.counterReview + 1;
+            $scope.$apply();
+        }
+
         function init() {
             var id = $routeParams.itemId;
             if (id > 0) {
